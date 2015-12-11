587c4603d3ab379e4ee22f2dcda7d7798cd35dcf
Damon Sauve <damonsauve@gmail.com>
2015-12-11 12:31:33 -0800

    fix spacing around arguments

--- db_credentials/DBCredentials.py
+++ db_credentials/DBCredentials.py
@@ -5,7 +5,7 @@ import re
 
 class DBCredentials:
 
-    def __init__( self ):
+    def __init__(self):
 
         self.creds = {
             'host':'',
@@ -19,15 +19,15 @@ class DBCredentials:
 
     # Load credentials from a file: no input validation.
     #
-    def load_file( self, filename ):
+    def load_file(self, filename):
 
-        f = open( filename, 'r' )
+        f = open(filename, 'r')
         text = f.read()
         f.close
 
         #print text
 
-        tuples = re.findall( r'(\w+)=([^\s]+)', text )
+        tuples = re.findall(r'(\w+)=([^\s]+)', text)
 
         #print tuples
         #[('host', 'localhost'), ('username', 'foo'), ('password', 'bar')]
@@ -39,42 +39,42 @@ class DBCredentials:
 
         return
 
-    def get_host( self ):
+    def get_host(self):
         return self.creds['host']
 
-    def set_host( self, host ):
+    def set_host(self, host):
         self.creds['host'] = host
 
     # listener port - return if specified, otherwise default to 3306
     #
-    def get_port( self ):
+    def get_port(self):
         if self.creds['port']:
             return self.creds['port']
         else:
             return '3306'
 
-    def set_port( self, port ):
+    def set_port(self, port):
         self.creds['port'] = port
 
-    def get_username( self ):
+    def get_username(self):
         return self.creds['username']
 
-    def set_username( self, username ):
+    def set_username(self, username):
         self.creds['sid'] = username
 
-    def get_password( self ):
+    def get_password(self):
         return self.creds['password']
 
-    def set_password( self, password ):
+    def set_password(self, password):
         self.creds['password'] = password
 
     # database
     #
-    def get_database( self ):
+    def get_database(self):
         if self.creds['database'] == '' and self.creds['host'] != '':
             self.creds['database'] = self.creds['host']
 
         return self.creds['database']
 
-    def set_database( self, database ):
+    def set_database(self, database):
         self.creds['database'] = database
